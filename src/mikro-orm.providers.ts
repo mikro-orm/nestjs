e51206762f9eb3e96bfc9edbb6abbf7ae8bc08a8
Brett Striker <StrikeForceZero@gmail.com>
2021-08-19 14:21:16 -0400

    feat: add support for nestjs@8.0.0 (#29)
    Co-authored-by: Martin Adámek <banan23@gmail.com>

--- src/mikro-orm.providers.ts
+++ src/mikro-orm.providers.ts
@@ -1,8 +1,8 @@
 import { getRepositoryToken, logger, MIKRO_ORM_MODULE_OPTIONS, REGISTERED_ENTITIES } from './mikro-orm.common';
-import { AnyEntity, ConfigurationLoader, EntityManager, EntityName, MikroORM } from '@mikro-orm/core';
+import { AnyEntity, ConfigurationLoader, EntityManager, EntityName, MikroORM, EntityManagerType, IDatabaseDriver, MetadataStorage } from '@mikro-orm/core';
 
 import { MikroOrmModuleAsyncOptions, MikroOrmModuleOptions, MikroOrmOptionsFactory } from './typings';
-import { Provider, Scope } from '@nestjs/common';
+import { Provider, Scope, Type } from '@nestjs/common';
 
 export const createMikroOrmProvider = (): Provider => ({
   provide: MikroORM,
@@ -26,8 +26,10 @@ export const createMikroOrmProvider = (): Provider => ({
   inject: [MIKRO_ORM_MODULE_OPTIONS],
 });
 
-export const createMikroOrmEntityManagerProvider = (scope = Scope.DEFAULT, alias?: string): Provider => ({
-  provide: alias ?? EntityManager,
+export type EntityManagerProvider = Provider<IDatabaseDriver[typeof EntityManagerType] & EntityManager>;
+
+export const createMikroOrmEntityManagerProvider = (scope = Scope.DEFAULT, entityManager: Type<EntityManager> = EntityManager): EntityManagerProvider => ({
+  provide: entityManager,
   scope,
   useFactory: (orm: MikroORM) => scope === Scope.DEFAULT ? orm.em : orm.em.fork(),
   inject: [MikroORM],
@@ -71,9 +73,26 @@ export const createAsyncProviders = (options: MikroOrmModuleAsyncOptions): Provi
 };
 
 export const createMikroOrmRepositoryProviders = (entities: EntityName<AnyEntity>[]): Provider[] => {
-  return (entities || []).map(entity => ({
-    provide: getRepositoryToken(entity),
-    useFactory: (em: EntityManager) => em.getRepository(entity),
-    inject: [EntityManager],
-  }));
+  const metadata = Object.values(MetadataStorage.getMetadata());
+  const providers: Provider[] = [];
+
+  (entities || []).forEach(entity => {
+    const meta = metadata.find(meta => meta.class === entity);
+
+    if (meta?.customRepository) {
+      providers.push({
+        provide: meta.customRepository(),
+        useFactory: em => em.getRepository(entity),
+        inject: [EntityManager],
+      });
+    }
+
+    providers.push({
+      provide: getRepositoryToken(entity),
+      useFactory: em => em.getRepository(entity),
+      inject: [EntityManager],
+    });
+  });
+
+  return providers;
 };
