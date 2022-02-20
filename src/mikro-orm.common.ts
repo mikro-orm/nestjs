d5de7e6b0b15244166360dedb75f1334916ce54e
Steven Tsang <tsangste@gmail.com>
2022-02-20 11:52:29 +0000

    remove the use of 'default' from getting injection tokens and change getRepository to work without using contextNames

--- src/mikro-orm.common.ts
+++ src/mikro-orm.common.ts
@@ -7,15 +7,18 @@ export const REGISTERED_ENTITIES = new Set<EntityName<AnyEntity>>();
 export const CONTEXT_NAMES = new Array<string>();
 export const logger = new Logger(MikroORM.name);
 
-export const getMikroORMToken = (name = 'default') => `${name}_MikroORM`;
+export const getMikroORMToken = (name: string) => `${name}_MikroORM`;
 export const InjectMikroORM = (name: string) => Inject(getMikroORMToken(name));
 
-export const getEntityManagerToken = (name = 'default') => `${name}_EntityManager`;
+export const getEntityManagerToken = (name: string) => `${name}_EntityManager`;
 export const InjectEntityManager = (name: string) => Inject(getEntityManagerToken(name));
-export const getSqlEntityManagerToken = (name = 'default') => `${name}_SqlEntityManager`;
+export const getSqlEntityManagerToken = (name: string) => `${name}_SqlEntityManager`;
 export const InjectSqlEntityManager = (name: string) => Inject(getSqlEntityManagerToken(name));
-export const getMongoEntityManagerToken = (name = 'default') => `${name}_MongoEntityManager`;
+export const getMongoEntityManagerToken = (name: string) => `${name}_MongoEntityManager`;
 export const InjectMongoEntityManager = (name: string) => Inject(getMongoEntityManagerToken(name));
 
-export const getRepositoryToken = <T> (entity: EntityName<T>, name = 'default') => `${Utils.className(entity)}Repository_${name}`;
+export const getRepositoryToken = <T> (entity: EntityName<T>, name?: string) => {
+  const suffix = name ? `_${name}` : '';
+  return `${Utils.className(entity)}Repository${suffix}`;
+};
 export const InjectRepository = <T> (entity: EntityName<T>, name: string) => Inject(getRepositoryToken(entity, name));
