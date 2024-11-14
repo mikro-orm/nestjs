7b06279273996de991371a6702d7e302b0d476b9
Steven Tsang <tsangste@gmail.com>
2024-11-14 20:36:35 +0000

    feat: switch to use new `InjectMikroORMs` decorator in `MultipleMikroOrmMiddleware`

--- src/multiple-mikro-orm.middleware.ts
+++ src/multiple-mikro-orm.middleware.ts
@@ -1,10 +1,11 @@
 import { RequestContext, type MikroORM } from '@mikro-orm/core';
-import { Inject, Injectable, type NestMiddleware } from '@nestjs/common';
+import { Injectable, type NestMiddleware } from '@nestjs/common';
+import { InjectMikroORMs } from './mikro-orm.common';
 
 @Injectable()
 export class MultipleMikroOrmMiddleware implements NestMiddleware {
 
-  constructor(@Inject('MikroORMs') private readonly orm: MikroORM[]) {}
+  constructor(@InjectMikroORMs() private readonly orm: MikroORM[]) {}
 
   use(req: unknown, res: unknown, next: (...args: any[]) => void) {
     RequestContext.create(this.orm.map(orm => orm.em), next);
