fe1f248b3f0e65b57cefb60736f08eb3dffe4a6c
Martin Adámek <banan23@gmail.com>
2025-02-16 22:40:27 +0100

    fix: use named wildcards in middleware route in nest v11
    Closes #205

--- src/middleware.helper.ts
+++ src/middleware.helper.ts
@@ -1,15 +1,23 @@
 import type { MikroOrmMiddlewareModuleOptions, NestMiddlewareConsumer } from './typings';
-import type { MiddlewareConsumer } from '@nestjs/common';
+import { type MiddlewareConsumer, HttpStatus } from '@nestjs/common';
 
 export function forRoutesPath(options: MikroOrmMiddlewareModuleOptions, consumer: MiddlewareConsumer) {
-  const isNestMiddleware = (consumer: MiddlewareConsumer): consumer is NestMiddlewareConsumer => {
-    return typeof (consumer as any).httpAdapter === 'object';
+  if (options.forRoutesPath) {
+    return options.forRoutesPath;
+  }
+
+  // detect nest v11 based on a newly added enum value
+  if (HttpStatus.MULTI_STATUS) {
+    return '{*all}';
+  }
+
+  const isFastify = (consumer: MiddlewareConsumer) => {
+    if (typeof (consumer as any).httpAdapter !== 'object') {
+      return false;
+    }
+
+    return (consumer as NestMiddlewareConsumer).httpAdapter.constructor.name.toLowerCase().startsWith('fastify');
   };
 
-  const usingFastify = (consumer: NestMiddlewareConsumer) => {
-    return consumer.httpAdapter.constructor.name.toLowerCase().startsWith('fastify');
-  };
-
-  return options.forRoutesPath ??
-    (isNestMiddleware(consumer) && usingFastify(consumer) ? '(.*)' : '*');
+  return isFastify(consumer) ? '(.*)' : '*';
 }
