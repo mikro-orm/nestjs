1d2f63146e704f50000d2b7afac6b68e491b6488
Martin Adámek <banan23@gmail.com>
2025-12-20 14:00:06 +0100

    test: fix tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,26 +2,14 @@ import { defineConfig } from 'vitest/config';
 import swc from 'unplugin-swc';
 
 export default defineConfig({
-  esbuild: {
-    target: 'es2024',
-    keepNames: true,
-  },
   plugins: [
     swc.vite({
-      jsc: {
-        target: 'es2024',
-        parser: {
-          syntax: 'typescript',
-          decorators: true,
-        },
-        transform: {
-          decoratorVersion: '2022-03',
-        },
-      },
+      jsc: { target: 'es2024' },
       sourceMaps: true,
     }),
   ],
   test: {
+    environment: 'node',
     globals: true,
     coverage: {
       reporter: ['clover', 'json', 'lcov', 'text'],
@@ -31,6 +19,5 @@ export default defineConfig({
     clearMocks: true,
     isolate: false,
     testTimeout: 60_000,
-    hookTimeout: 60_000,
   },
 });
